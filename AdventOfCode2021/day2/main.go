51e5f70fb3989ec161cfb6ffe65f9f269d48b3cb
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2021-12-02 08:27:20 -0330

    reformat

--- AdventOfCode2021/day2/main.go
+++ AdventOfCode2021/day2/main.go
@@ -11,49 +11,6 @@ import (
 
 var horizontal, depth, aim int
 
-func part1(command string, value int) {
-	switch command {
-	case "up":
-		{
-			if depth > 0 {
-				depth -= value
-			}
-			break
-		}
-	case "down":
-		{
-			depth += value
-			break
-		}
-	case "forward":
-		{
-			horizontal += value
-			break
-		}
-	}
-}
-
-func part2(command string, value int) {
-	switch command {
-	case "up":
-		{
-			aim -= value
-			break
-		}
-	case "down":
-		{
-			aim += value
-			break
-		}
-	case "forward":
-		{
-			horizontal += value
-			depth += aim * value
-			break
-		}
-	}
-}
-
 func main() {
 	horizontal, depth, aim = 0, 0, 0
 	f, err := os.Open("input.txt")
@@ -73,9 +30,28 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		// part1(command, value)
-		part2(command, value)
+		switch command {
+		case "up":
+			{
+				aim -= value
+				break
+			}
+		case "down":
+			{
+				aim += value
+				break
+			}
+		case "forward":
+			{
+				horizontal += value
+				depth += aim * value
+				break
+			}
+		}
 	}
 	// Part 1 print
+	fmt.Println((horizontal * aim))
+
+	// Part 2 print
 	fmt.Println((horizontal * depth))
 }
