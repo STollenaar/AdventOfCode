ebb7c55b33433935c48a75f445e47d70638a4b13
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2021-12-16 09:16:33 -0330

    start rewrite

--- AdventOfCode2021/day14/main.go
+++ AdventOfCode2021/day14/main.go
@@ -4,14 +4,92 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strings"
-	"sync"
 	"time"
 )
 
-type InsertionRule struct {
-	par1, par2, insertChar string
+type Pair struct {
+	pair, creationChar string
+	count              int
+}
+
+type Pairs []*Pair
+
+func (p *Pairs) addPair(pair *Pair) {
+	*p = append(*p, pair)
+}
+
+func (p *Pairs) createPair(pair *Pair) []Pair {
+	pairChars := strings.Split(pair.pair, "")
+
+	pair1 := pairChars[0] + pair.creationChar
+	pair2 := pair.creationChar + pairChars[1]
+
+	pairs1 := Pair{pair: pair1, count: pair.count}
+	pairs2 := Pair{pair: pair2, count: pair.count}
+	pairReduce := *pair
+	pairReduce.count = -pair.count
+
+	return []Pair{pairs1, pairs2, pairReduce}
+}
+
+// Get amount of character occurences for all pairs
+func (p *Pairs) getOccurences(char string) (occurences int) {
+	oc := make(map[string]int)
+
+	for _, pair := range *p {
+		elements := strings.Split(pair.pair, "")
+		oc[elements[0]]++
+		oc[elements[1]]++
+	}
+	return oc[char]
+}
+
+func (p *Pairs) getMinElement() (occurence int) {
+	elements := p.getElementsAmount()
+
+	occurence = math.MaxInt32
+	for _, el := range elements {
+		if el < occurence {
+			occurence = el
+		}
+	}
+	return occurence
+}
+
+func (p *Pairs) getElementsAmount() (elements map[string]int) {
+	elements = make(map[string]int)
+	elementOccurences := make(map[string]int)
+	for _, pair := range *p {
+		pairChars := strings.Split(pair.pair, "")
+		elements[pairChars[0]] += pair.count
+		elements[pairChars[1]] += pair.count
+
+		if elementOccurences[pairChars[0]] == 0 {
+			elementOccurences[pairChars[0]] = p.getOccurences(pairChars[0])
+		}
+		if elementOccurences[pairChars[1]] == 0 {
+			elementOccurences[pairChars[1]] = p.getOccurences(pairChars[1])
+		}
+	}
+	for key := range elements {
+		elements[key] /= elementOccurences[key]
+	}
+
+	return elements
+}
+
+func (p *Pairs) getMaxElement() (occurence int) {
+	elements := p.getElementsAmount()
+
+	for _, el := range elements {
+		if el > occurence {
+			occurence = el
+		}
+	}
+	return occurence
 }
 
 func main() {
@@ -27,8 +105,9 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	var polymer []string
-	var insertionRules []InsertionRule
+	// var polymer []string
+	pairs := new(Pairs)
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
@@ -38,10 +117,21 @@ func main() {
 
 		if strings.Contains(line, "->") {
 			insertions := strings.Split(line, " -> ")
-			searchPar := strings.Split(insertions[0], "")
-			insertionRules = append(insertionRules, InsertionRule{par1: searchPar[0], par2: searchPar[1], insertChar: insertions[1]})
+
+			if pair := inSlice(pairs, insertions[0]); pair != nil && pair.creationChar == "" {
+				pair.creationChar = insertions[1]
+			} else if pair == nil {
+				pairs.addPair(&Pair{pair: insertions[0], creationChar: insertions[1]})
+			}
 		} else {
-			polymer = strings.Split(line, "")
+			polymer := strings.Split(line, "")
+			for i := 0; i < len(polymer)-1; i++ {
+				if pair := inSlice(pairs, polymer[i]+polymer[i+1]); pair == nil {
+					pairs.addPair(&Pair{pair: polymer[i] + polymer[i+1], count: 1})
+				} else {
+					pair.count++
+				}
+			}
 		}
 
 		if err != nil {
@@ -50,118 +140,37 @@ func main() {
 	}
 
 	for steps := 0; steps < 40; steps++ {
-		fmt.Println("Doing step: ", steps)
-		polymerCopy := new(sync.Map)
-		partitions := chunks(polymer, 100000)
-		fmt.Println("Looping through partitions on step: ", steps)
-		// var waitGroup sync.WaitGroup
-		for p, partition := range partitions {
-			// waitGroup.Add(1)
-			// go func(polymerCopy *sync.Map, p int, partition []string, waitGroup *sync.WaitGroup) {
-			// 	defer waitGroup.Done()
-			for i := 0; i < len(partition)-1; i++ {
-				slice, _ := polymerCopy.LoadOrStore(p, []string{})
-
-				slice = append(slice.([]string), partition[i])
-				polymerCopy.Store(p, slice)
-				for _, insertionRule := range insertionRules {
-					if partition[i] == insertionRule.par1 && partition[i+1] == insertionRule.par2 {
-						slice = append(slice.([]string), insertionRule.insertChar)
-
-						polymerCopy.Store(p, slice)
-						break
-					}
-				}
+		var changes []Pair
+		for i, pair := range *pairs {
+			if pair.count > 0 {
+				changes = append(changes, pairs.createPair((*pairs)[i])...)
 			}
-			// }(polymerCopy, p, partition, &waitGroup)
 		}
-		// waitGroup.Wait()
-		var polCopy []string
-
-		fmt.Println("Merging into polymer slice for step: ", steps)
-		for i := 0; i < len(partitions); i++ {
-			slice, _ := polymerCopy.Load(i)
-			polCopy = append(polCopy, slice.([]string)...)
+		for _, pair := range changes {
+			inSlice(pairs, pair.pair).count += pair.count
 		}
-		polymer = polCopy
 
 		if steps == 9 {
-			mostCommon := getMostCommonAmount(polymer)
-			leastCommon := getLeastCommonAmount(polymer, mostCommon)
+			mostCommon := pairs.getMaxElement()
+			leastCommon := pairs.getMinElement()
 			elapsed := time.Since(start)
 			fmt.Println("Execution time for part 1: ", elapsed)
 			fmt.Println("quantity of polymer for par 1: ", mostCommon-leastCommon)
 		}
 	}
-	mostCommon := getMostCommonAmount(polymer)
-	leastCommon := getLeastCommonAmount(polymer, mostCommon)
+	mostCommon := pairs.getMaxElement()
+	leastCommon := pairs.getMinElement()
 	elapsed := time.Since(start)
 	fmt.Println("Execution time for part 2: ", elapsed)
 	fmt.Println("quantity of polymer for par 2: ", mostCommon-leastCommon)
 
 }
 
-func chunks(xs []string, chunkSize int) [][]string {
-	if len(xs) == 0 {
-		return nil
-	}
-	divided := make([][]string, (len(xs)+chunkSize-1)/chunkSize)
-	prev := 0
-	i := 0
-	till := len(xs) - chunkSize
-	for prev < till {
-		next := prev + chunkSize
-		divided[i] = xs[prev : next+1]
-		prev = next
-		i++
-	}
-	divided[i] = xs[prev:]
-	return divided
-}
-
-func getMostCommonAmount(slice []string) (amount int) {
-	var seenElements []string
-	for _, el := range slice {
-		if !inSlice(seenElements, el) {
-			occurences := getOccurences(slice, el)
-			if occurences > amount {
-				amount = occurences
-			}
-			seenElements = append(seenElements, el)
+func inSlice(slice *Pairs, pair string) *Pair {
+	for _, el := range *slice {
+		if el.pair == pair {
+			return el
 		}
 	}
-	return amount
-}
-
-func getLeastCommonAmount(slice []string, mostCommon int) (amount int) {
-	amount = mostCommon
-	var seenElements []string
-	for _, el := range slice {
-		if !inSlice(seenElements, el) {
-			occurences := getOccurences(slice, el)
-			if occurences < amount {
-				amount = occurences
-			}
-			seenElements = append(seenElements, el)
-		}
-	}
-	return amount
-}
-
-func inSlice(slice []string, par string) bool {
-	for _, el := range slice {
-		if el == par {
-			return true
-		}
-	}
-	return false
-}
-
-func getOccurences(slice []string, par string) (amount int) {
-	for _, el := range slice {
-		if el == par {
-			amount++
-		}
-	}
-	return amount
+	return nil
 }
