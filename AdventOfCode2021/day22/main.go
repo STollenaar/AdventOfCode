29a0d0c1452f93cee7d20adeb633f60d6179c6a2
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2021-12-23 13:44:31 -0330

    rewrite of day 22 and done

--- AdventOfCode2021/day22/main.go
+++ AdventOfCode2021/day22/main.go
@@ -10,9 +10,56 @@ import (
 	"time"
 )
 
-type Step struct {
-	onOff                              bool
-	xMin, xMax, yMin, yMax, zMin, zMax int
+type Cube struct {
+	on                                         bool
+	xMin, xMax, yMin, yMax, zMin, zMax, volume int64
+}
+
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(a int64) int64 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func (c Cube) doIntersect(other Cube) Cube {
+	var intersect Cube
+	intersect.on = !other.on
+
+	intersect.xMin = max(other.xMin, c.xMin)
+	intersect.xMax = min(other.xMax, c.xMax)
+
+	intersect.yMin = max(other.yMin, c.yMin)
+	intersect.yMax = min(other.yMax, c.yMax)
+
+	intersect.zMin = max(other.zMin, c.zMin)
+	intersect.zMax = min(other.zMax, c.zMax)
+
+	intersect.volume = (abs(intersect.xMin-intersect.xMax) + 1) * (abs(intersect.yMin-intersect.yMax) + 1) * (abs(intersect.zMin-intersect.zMax) + 1)
+
+	return intersect
+}
+
+func (c Cube) isValidPoint() bool {
+	return c.xMin <= c.xMax && c.yMin <= c.yMax && c.zMin <= c.zMax
+}
+
+func (c Cube) isInitPoint() bool {
+	return c.xMin >= -50 && c.xMax <= 50 && c.yMin >= -50 && c.yMax <= 50 && c.zMin >= -50 && c.zMax <= 50
 }
 
 func main() {
@@ -28,92 +75,99 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	var steps []Step
+	var cubes []Cube
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), " ")
 
-		var onOff bool
+		var on bool
 		if line[0] == "on" {
-			onOff = true
+			on = true
 		} else {
-			onOff = false
+			on = false
 		}
 		area := strings.Split(line[1], ",")
 
 		xArea := strings.Split(strings.ReplaceAll(area[0], "x=", ""), "..")
-		xMin, _ := strconv.Atoi(xArea[0])
-		xMax, _ := strconv.Atoi(xArea[1])
+		xMin, _ := strconv.ParseInt(xArea[0], 10, 64)
+		xMax, _ := strconv.ParseInt(xArea[1], 10, 64)
 
 		yArea := strings.Split(strings.ReplaceAll(area[1], "y=", ""), "..")
-		yMin, _ := strconv.Atoi(yArea[0])
-		yMax, _ := strconv.Atoi(yArea[1])
+		yMin, _ := strconv.ParseInt(yArea[0], 10, 64)
+		yMax, _ := strconv.ParseInt(yArea[1], 10, 64)
 
 		zArea := strings.Split(strings.ReplaceAll(area[2], "z=", ""), "..")
-		zMin, _ := strconv.Atoi(zArea[0])
-		zMax, _ := strconv.Atoi(zArea[1])
+		zMin, _ := strconv.ParseInt(zArea[0], 10, 64)
+		zMax, _ := strconv.ParseInt(zArea[1], 10, 64)
 
-		steps = append(steps, Step{onOff: onOff, xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax, zMin: zMin, zMax: zMax})
+		volume := (abs(xMin-xMax) + 1) * (abs(yMin-yMax) + 1) * (abs(zMin-zMax) + 1)
+		cubes = append(cubes, Cube{on: on, xMin: xMin, xMax: xMax, yMin: yMin, yMax: yMax, zMin: zMin, zMax: zMax, volume: volume})
 	}
 
-	doPart1(steps, start)
+	doPart1(cubes, start)
 	start = time.Now()
-	doPart2(steps, start)
+	doPart2(cubes, start)
 }
 
-func doPart1(steps []Step, start time.Time) {
-	reactor := make(map[string]bool)
+func doPart1(cubes []Cube, start time.Time) {
 
-	for _, step := range steps {
-		for x := step.xMin; x <= step.xMax; x++ {
-			if x < -50 || x > 50 {
-				break
+	var amount int64
+	var checkedCubes []Cube
+
+	for _, cube := range cubes {
+		if cube.isInitPoint() {
+			var nextCubes []Cube
+			if cube.on {
+				nextCubes = append(nextCubes, cube)
 			}
-			for y := step.yMin; y <= step.yMax; y++ {
-				if y < -50 || y > 50 {
-					break
-				}
-
-				for z := step.zMin; z <= step.zMax; z++ {
-					if z < -50 || z > 50 {
-						break
-					}
-					reactor[strconv.Itoa(x)+"_"+strconv.Itoa(y)+"_"+strconv.Itoa(z)] = step.onOff
+			for _, check := range checkedCubes {
+				if intersect := cube.doIntersect(check); intersect.isValidPoint() {
+					nextCubes = append(nextCubes, intersect)
 				}
 			}
+			checkedCubes = append(checkedCubes, nextCubes...)
 		}
 	}
-	var amount int
-	for _, value := range reactor {
-		if value {
-			amount++
+
+	for _, checkedCube := range checkedCubes {
+		if checkedCube.on {
+			amount += checkedCube.volume
+		} else {
+			amount -= checkedCube.volume
 		}
 	}
+
 	elapsed := time.Since(start)
 	fmt.Println("Execution time for part 1: ", elapsed)
 	fmt.Println("Amount on for part 1: ", amount)
 
 }
 
-func doPart2(steps []Step, start time.Time) {
-	reactor := make(map[string]bool)
+func doPart2(cubes []Cube, start time.Time) {
 
-	for _, step := range steps {
-		for x := step.xMin; x <= step.xMax; x++ {
-			for y := step.yMin; y <= step.yMax; y++ {
-				for z := step.zMin; z <= step.zMax; z++ {
-					reactor[strconv.Itoa(x)+"_"+strconv.Itoa(y)+"_"+strconv.Itoa(z)] = step.onOff
-				}
+	var amount int64
+	var checkedCubes []Cube
+	for _, cube := range cubes {
+		var nextCubes []Cube
+		if cube.on {
+			nextCubes = append(nextCubes, cube)
+		}
+		for _, check := range checkedCubes {
+			if intersect := cube.doIntersect(check); intersect.isValidPoint() {
+				nextCubes = append(nextCubes, intersect)
 			}
 		}
+		checkedCubes = append(checkedCubes, nextCubes...)
 	}
-	var amount int
-	for _, value := range reactor {
-		if value {
-			amount++
+
+	for _, checkedCube := range checkedCubes {
+		if checkedCube.on {
+			amount += checkedCube.volume
+		} else {
+			amount -= checkedCube.volume
 		}
 	}
+
 	elapsed := time.Since(start)
 	fmt.Println("Execution time for part 2: ", elapsed)
 	fmt.Println("Amount on for part 2: ", amount)
-
 }
